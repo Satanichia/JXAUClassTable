9b94b08627e586a8d881fa72b3a7f54fba423efe
Satanichia <Sakilia.Hallon@gmail.com>
2018-05-30 16:09:57 +0800

    Login screeen

---
+++ ClassTable/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  ClassTable
+//
+//  Created by Satanichia on 2018/5/30.
+//  Copyright © 2018年 Satanichia. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewCotroller : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+    
+}
